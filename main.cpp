915b46e062e817f69dba138caccf56d61ee59ddc
hyunmindev <jung@hyunmin.dev>
2020-11-14 22:04:40 +0900

    좋았어 오늘은
    여기까지

--- main.cpp
+++ main.cpp
@@ -7,15 +7,41 @@
 
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
+
+
+vector<pair<int, int>> makePairVector(int number, vector<int>& color, vector<int>& size)
+{
+  vector<pair<int, int>> size_color_pair(number);
+  
+  for(int i = 0; i < number; i++)
+  {
+    size_color_pair[i] = make_pair(size[i], color[i]);
+  }
+  return size_color_pair;
+}
+
 
 
 vector<int> getOutput(int number, vector<int>& color, vector<int>& size)
 {
   vector<int> output(number);
   
+  vector<pair<int, int>> size_color_pair = makePairVector(number, color, size);
+  
+  sort(size_color_pair.begin(), size_color_pair.end());
+  
+  for(int i = 0 ; i < number; i++)
+  {
+//    cout << size_color_pair[i].first << ", " << size_color_pair[i].second << endl;
+    for (int j = i + 1; j < number; j++)
+    {
 
+    }
+  }
+  
   
   return output;
 }
